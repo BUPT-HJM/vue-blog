cf1299a9a16bef1858ada6f352d80801ff5774d9
HJM <452114240@qq.com>
2016-12-23 15:52:03 +0800

    test koa-router && use koa-router 7.x for koa 2.x!!!（bother me a lot)

--- server/api/routes/router-test.js
+++ server/api/routes/router-test.js
@@ -1,8 +1,19 @@
 // test router
 export default (router) => {
     router.get('/123', ctx => {
-        ctx.body = '123';
-        console.log('123!');
+        // console.log(this);
+        // ctx.type="text/plain;charset=utf-8";
+        ctx.body = { a: 123 };
+        // ctx.status = 200;
+        // ctx.state = {
+        //     title: 'aaa'
+        // };
+
+        //ctx.throw(500,123)
+        console.log(ctx.res);
+        // console.log(ctx.req);
+
+        // console.log(ctx);
+        // console.log('123!');
     });
 }
-
---
+++ server/koa-router-test.js
@@ -0,0 +1,28 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+const logger = require('koa-logger');
+const bodyParser = require('koa-bodyparser');
+const convert = require('koa-convert');
+const json = require('koa-json');
+
+
+const app = new Koa();
+app.use(convert.compose([
+    bodyParser(),
+    json(),
+    logger()
+]));
+const router = new Router();
+router.get('/', (ctx, next) => {
+    ctx.body = { a: 1 };
+    console.log(ctx);
+});
+app.use(convert(router.routes()));
+app.use(convert(router.allowedMethods()));
+
+
+create server
+app.listen(3001, () => {
+    console.log('The server is running at http://localhost:' + 3001);
+});
+
