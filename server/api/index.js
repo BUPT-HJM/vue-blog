3e88f9ae093b7d8386d21891cade0dd60c0b2c39
huangjunming <huangjunming@bytedance.com>
2017-11-02 16:40:33 +0800

    fix: require-dir bug: https://github.com/aseemk/requireDir/commit/7fca87ce9659e920675460a81f9e61f56e
    fix #10

--- server/api/index.js
+++ server/api/index.js
@@ -1,16 +1,16 @@
 import compose from 'koa-compose';
 import Router from 'koa-router';
 import convert from 'koa-convert';
-import importDir from 'import-dir';
 import config from '../configs';
-const routes = importDir('./routes');
+import requireDir from 'require-dir';
+const routes = requireDir('./routes');
 
 export default function api() {
   const router = new Router({
     prefix: config.app.baseApi
   });
   Object.keys(routes).forEach(name => {
-    return routes[name](router)
+    return routes[name]['default'](router)
   });
   return convert.compose([
     router.routes(),
